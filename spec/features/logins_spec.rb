0a2edb4f0e6471dfe175bd450b0ebb98e5920328
Jason Wright <noobjey@gmail.com>
2015-10-27 12:42:01 -0600

    stub oauth login and test login page

--- spec/features/logins_spec.rb
+++ spec/features/logins_spec.rb
@@ -1,12 +1,48 @@
 require 'rails_helper'
+require 'support/login_helper'
 
 RSpec.feature "Logins", type: :feature do
-  describe "a user visits the login page" do
-    it "sees a login button" do
-      visit root_path
 
-      expect(current_path).to eq(root_path)
-      expect(page).to have_button("Login")
+  include Capybara::DSL
+  include LoginHelper
+
+  before do
+    Capybara.app = Noobtwit::Application
+    stub_omniauth()
+  end
+
+  describe "a user that visits the login page" do
+    let(:login_button) { "Login with Twitter" }
+
+    before do
+      visit root_path
+    end
+
+    context "and is not logged in" do
+
+      it "sees a login button" do
+        expect(current_path).to eq root_path
+        expect(page).to have_link login_button
+      end
+
+      it "can login" do
+        click_on login_button
+
+        expect(current_path).to eq dashboard_path
+        expect(page).to have_content "Welcome"
+      end
+    end
+
+
+    context "and has already logged in" do
+
+      before do
+        login_user()
+      end
+
+      it "sees their dashboard" do
+        expect(current_path).to eq(dashboard_path)
+      end
     end
   end
 end
