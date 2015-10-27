013b4b3e139bac3750e209f94184c84cb08aa429
Jason Wright <noobjey@gmail.com>
2015-10-26 21:15:32 -0600

    added link to twitter oauth and callback route

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,4 +2,9 @@ class SessionController < ApplicationController
   def new
 
   end
+
+  def create
+    byebug
+    render text: request.env["omniauth.auth"].inspect
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'session#new'
+
+  # Special login path for OmniAuth
+  # todo: link to reference
+  get '/auth/twitter', as: :login
+
+  get '/auth/twitter/callback', to: 'session#create'
 end
