58e3423523c2b8d2450b1c570fa1f2e49e6251a6
Jason Wright <noobjey@gmail.com>
2015-10-27 22:40:31 -0600

    refactor test

--- spec/services/twitter_services_spec.rb
+++ spec/services/twitter_services_spec.rb
@@ -6,35 +6,24 @@ RSpec.feature "TwitterServices:", type: :feature do
 
   before do
     @user ||= create_twitter_user
+    @service = TwitterService.new(@user)
   end
 
   it "#tweet_count" do
     VCR.use_cassette('twitter user info') do
-      service = TwitterService.new(@user)
-
-      user_tweet_count = service.my_tweet_count
-
-      expect(user_tweet_count).to eq(88)
+      expect(@service.my_tweet_count).to eq(88)
     end
   end
 
   it "#followers" do
     VCR.use_cassette('twitter user info') do
-      service = TwitterService.new(@user)
-
-      followers = service.followers
-
-      expect(followers).to eq(54)
+      expect(@service.followers).to eq(54)
     end
   end
 
   it "#following" do
     VCR.use_cassette('twitter user info') do
-      service = TwitterService.new(@user)
-
-      following = service.following
-
-      expect(following).to eq(65)
+      expect(@service.following).to eq(65)
     end
   end
 
